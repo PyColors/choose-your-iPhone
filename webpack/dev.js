3e3d7b169da4f7497e10f435f42bfca95f016f45
PyColors <patweb44@gmail.com>
2018-11-03 18:45:01 +0000

    Remove duplicate contentBase key from Webpack

--- webpack/dev.js
+++ webpack/dev.js
@@ -5,14 +5,14 @@ const webpack = require('webpack')
 const config = require('./webpack.dev.js')
 const chokidar = require('chokidar')
 const webpackDevServer = require('webpack-dev-server')
-const port = 8080;
+const port = 8080
 
 // Add compiler
-const compiler = webpack(config);
+const compiler = webpack(config)
 const hotMiddleWare = require('webpack-hot-middleware')(compiler)
 
 // Listen all HTML files
-chokidar.watch('./*.html').on('all', function() {
+chokidar.watch('./*.html').on('all', function () {
   hotMiddleWare.publish({ action: 'reload' })
 })
 
@@ -27,23 +27,21 @@ let server = new webpackDevServer(compiler, {
   * As I am using Cloud9 ide, this is for the "Invalid Host header", HTTPS
   * You must have to change it
   */
-  "public": "vodafone-pycolors.c9users.io", // - no trailing slash
-  contentBase: './',
+  'public': 'vodafone-pycolors.c9users.io', // - no trailing slash
   quiet: false,
   noInfo: false,
   publicPath: config.output.publicPath,
   stats: { colors: true }
-});
+})
 
 // Add hot-middleware to the server (MHR)
 // Can see Errors on the front end
 server.use(hotMiddleWare)
 
-server.listen(port, function(err) {
+server.listen(port, function (err) {
   if (err) {
-    console.log(err);
-  }
-  else {
-    console.log("Listen on the port: " + port)
+    console.log(err)
+  } else {
+    console.log('Listen on the port: ' + port)
   }
 })
