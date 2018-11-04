80883d890f9fe2bb16a64ee7c752278446a40f67
PyColors <patweb44@gmail.com>
2018-11-04 23:58:01 +0000

    Adding babel-preset-stage-0 npm

--- src/components/ui/paragraph/Paragraph.js
+++ src/components/ui/paragraph/Paragraph.js
@@ -1,17 +1,17 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class Paragraph extends Component {
-    
-  render() {
-    const { paragraph } = this.props;
 
-    return <p className="paragraph">{paragraph}</p>;
+  render () {
+    const { paragraph } = this.props
+
+    return <p className="paragraph">{paragraph}</p>
   }
 }
 
 Paragraph.propTypes = {
   paragraph: PropTypes.array.isRequired
-};
+}
 
-export default Paragraph;
+export default Paragraph
